5de582fadfbf3a2295850bed0282d65e975280bb
racidelaid <racidelaid@gmail.com>
2022-04-26 16:06:16 +0000

    fix linters

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,9 @@
 class UsersController < ApplicationController
-  def index
-  end
+  def index; end
 
-  def show
-  end
+  def show; end
 
-  def posts
-  end
+  def posts; end
 
-  def post
-  end
+  def post; end
 end
