4a964a774fd56235f5fe4fb4a314061c13e7e22e
racidelaid <racidelaid@gmail.com>
2022-04-26 16:03:06 +0000

    added all routes

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,13 @@
+class UsersController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def posts
+  end
+
+  def post
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root "main#index"
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get "/users", to: "users#index"
+  get "/users/:id", to: "users#show"
+  get "/users/:id/posts", to: "users#posts"
+  get "/users/:id/posts/:id", to: "users#post"
 end
