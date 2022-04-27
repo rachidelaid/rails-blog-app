993e095866ec7eb3adab60e7c7869791109685ee
racidelaid <racidelaid@gmail.com>
2022-04-27 14:26:24 +0000

    testing users index

---
+++ spec/requests/users_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe 'Users', type: :request do
+  describe 'GET #index' do
+    before(:each) do
+      get '/'
+    end
+
+    it 'GET requests response status was correct' do
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'response body includes correct placeholder text' do
+      expect(response.body).to include('a list of users')
+    end
+
+    it 'renders a correct template' do
+      expect(response).to render_template(:index)
+    end
+  end
+end
