f26d850eeb5e8dbd40ccbfbac2cb84eaa5132441
racidelaid <racidelaid@gmail.com>
2022-04-27 14:27:13 +0000

    testing users show

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -18,4 +18,22 @@ RSpec.describe 'Users', type: :request do
       expect(response).to render_template(:index)
     end
   end
+
+  describe 'GET #show' do
+    before(:each) do
+      get '/users/999'
+    end
+
+    it 'GET requests response status was correct' do
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'response body includes correct placeholder text' do
+      expect(response.body).to include('user page')
+    end
+
+    it 'renders a correct template' do
+      expect(response).to render_template(:show)
+    end
+  end
 end
