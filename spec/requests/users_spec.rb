8ed98569f06c71b23871f5b1c74c84b49dfbbd86
racidelaid <racidelaid@gmail.com>
2022-04-27 14:28:43 +0000

    testing users post

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -54,4 +54,22 @@ RSpec.describe 'Users', type: :request do
       expect(response).to render_template(:posts)
     end
   end
+
+  describe 'GET #post' do
+    before(:each) do
+      get '/users/999/posts/1'
+    end
+
+    it 'GET requests response status was correct' do
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'response body includes correct placeholder text' do
+      expect(response.body).to include('single post')
+    end
+
+    it 'renders a correct template' do
+      expect(response).to render_template(:post)
+    end
+  end
 end
