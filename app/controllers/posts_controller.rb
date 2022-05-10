0b4c51c68d6c2e8c4468b40259c1a989f15d4dd6
Pedro Guerreiro <pmoreiraguerreiro@gmail.com>
2022-05-10 16:15:48 +0100

    Add comment authorization

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  load_and_authorize_resource :except => :create
+  load_and_authorize_resource except: :create
 
   def index
     @user = User.find(params[:user_id])
@@ -7,7 +7,7 @@ class PostsController < ApplicationController
   end
 
   def show
-    @post = Post.find(params[:id])
+    @post = Post.find_by(id: params[:id])
     @user = User.find(params[:user_id])
   end
 
--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,21 +1,28 @@
-# frozen_string_literal: true
-
 class Ability
   include CanCan::Ability
 
   def initialize(user)
-    # user ||= User.new # guest user (not logged in)
-    # if user.admin?
-    #   can :manage, :all
-    # else
-    #   can :destroy, Post do |post|
-    #     post.author_id == user.id
-    #   end
+    user ||= User.new # guest user (not logged in)
+    if user.admin?
+      can :manage, :all
+    else
+      can :destroy, Post do |post|
+        post.author_id == user.id
+      end
 
-    #   can :create, Post
-    #   can :create, Comment
-    #   can :read, :all
-    # end
-    can :manage, :all
+      can :destroy, Comment do |comment|
+        comment.author_id == user.id
+      end
+
+      can :create, Post do |_post|
+        user.id
+      end
+
+      can :create, Comment do |_comment|
+        user.id
+      end
+
+      can :read, :all
+    end
   end
 end
