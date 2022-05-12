b8e26db0d9b2484fdbe30e69779f9dfe184cb989
racidelaid <racidelaid@gmail.com>
2022-05-12 14:25:53 +0100

    get all posts for a user endpoint

---
+++ app/controllers/api/posts_controller.rb
@@ -0,0 +1,11 @@
+class Api::PostsController < ApplicationController
+  def index
+    @posts = Post.where(author_id: params[:user_id])
+
+    if @posts.empty?
+      json_response({msg:"no post or user found"}, 400)
+    else
+      render json: @posts
+    end
+  end
+end
---
+++ app/controllers/api/users_controller.rb
@@ -0,0 +1,9 @@
+class Api::UsersController < ApplicationController
+  def index
+    json_response({msg:"welcome to our API"}, 400)
+  end
+  
+  def show
+    json_response({msg:"to see the posts add /posts"}, 400)
+  end
+end
